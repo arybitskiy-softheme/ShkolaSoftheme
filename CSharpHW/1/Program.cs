547544dea11c885f41a76816995adfce48f7b8ed
arybitskiy-softheme <alexander.rybitskiy@softheme.com>
2016-05-24 15:55:36 +0300

    myname

---
+++ CSharpHW/1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Alexander Rybitskiy";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("My name is {0}", name);
+            Console.ReadKey();
+        }
+    }
+}
